533d6e51f441d613f683b3bf8f211e532b69605d
Caleb Fynewever <caleb.fynewever@gmail.com>
2023-10-01 19:01:06 -0500

    actually made bugs for level10-15

--- find-the-bug/.levels/level10.java
+++ find-the-bug/.levels/level10.java
@@ -19,12 +19,12 @@ public class level10 {
 
     public static boolean checkPrime(int n) {
         if (n <= 1) {
-            return false;
+            return true;
         }
         
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-                return false;
+                return true;
             }
         }
         
--- find-the-bug/.levels/level12.java
+++ find-the-bug/.levels/level12.java
@@ -9,8 +9,8 @@ public class level12 {
 
     public static String reverseString(String input) {
         String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed += input.charAt(i);
+        for (int i = input.length() - 1; i >= 0; i++) {
+            reversed -= input.charAt(i);
         }
         return reversed;
     }
