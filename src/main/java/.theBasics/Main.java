3765b93d2c154c0dd2e3917aefb153acf1c69230
Luna07J <116044598+Luna07J@users.noreply.github.com>
2023-10-02 19:44:00 -0500

    my basics

--- src/main/java/.theBasics/Main.java
+++ src/main/java/.theBasics/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class Main {
     // Variable to store our instanced class for receiving input
     // NOTE: notice this variable has yet to be set equal to anything, this is done on line 13
@@ -7,11 +9,16 @@ public class Main {
     // Variable to store the users name once they provide it
     private static String name;
 
-    // This method is the singular starting point of our application
+    // This method is the singular starting poi nt of our application
     public static void main(String[] args) throws Exception {
-        // ASSIGNMENT A:
+        // ASSIGNMENT A:    
+        System.out.println("Hello World!");
         
         // ASSIGNMENT B:
+        input = new Scanner(System.in); 
+        System.out.println("Please enter your name below: ");
+        name = input.nextLine();
+        System.out.println("Welcome, "+name+"!");
         
         // ASSIGNMENT C:
     }
