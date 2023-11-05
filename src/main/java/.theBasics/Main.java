e3ab65e44765e39140c7c73602288bb6f6614acc
Afomia Mesfin <nia.msfn@gmail.com>
2023-11-04 22:45:15 -0500

    Assignment A/B for JavaTraining "The Basics"

--- src/main/java/.theBasics/Main.java
+++ src/main/java/.theBasics/Main.java
@@ -10,9 +10,12 @@ public class Main {
     // This method is the singular starting point of our application
     public static void main(String[] args) throws Exception {
         // ASSIGNMENT A:
-        
+        System.out.println("Hello world!");
         // ASSIGNMENT B:
-        
+        input = new Scanner(System.in);
+        System.out.println("Please enter your name below:");
+        name = input.nextLine();
+        System.out.println("Welcome," + name + "!");
         // ASSIGNMENT C:
     }
 }
